646d8340a9fc70e7202a8e34291aaeecb7f89e07
fansuregrin <pwz113436@gmail.com>
2023-07-27 17:22:50 +0800

    update model and add test_ie

--- models/model.py
+++ models/model.py
@@ -16,6 +16,7 @@ from torch.utils.data import DataLoader
 from torchvision.utils import save_image
 from tqdm import tqdm
 import cv2
+import shutil
 
 from .initialize import init_weight_bias
 from .networks import create_network
@@ -64,9 +65,10 @@ class BaseModel(ABC):
             if self.start_epoch == 0:
                 self.network.apply(init_weight_bias)
             else:
-                self.network.load_state_dict(torch.load("{}/weights_{:d}.pth".format(self.checkpoint_dir, self.start_epoch-1)))
+                weights_path = self.cfg['weights_path']
+                self.network.load_state_dict(torch.load("{}/{}".format(self.checkpoint_dir, weights_path)))
                 if self.logger:
-                    self.logger.info("Loaded model weights from epoch {:d}".format(self.start_epoch-1))
+                    self.logger.info("Loaded model weights from {}".format(weights_path))
         else:
             assert f"{self.mode} is not supported!"
 
@@ -94,11 +96,12 @@ class ImgEnhanceModel(BaseModel):
             self.checkpoint_dir = cfg['checkpoint_dir']
             self.result_dir = cfg['result_dir']
         
-    def load_weights(self, weights_fp: str):
-        self.network.load_state_dict(torch.load(weights_fp))
+    def load_weights(self, weights_path: str):
+        weights_path = os.path.join(self.checkpoint_dir, weights_path)
+        self.network.load_state_dict(torch.load(weights_path))
         if self.logger:
             self.logger.info('Loaded model weights from {}.'.format(
-                weights_fp
+                weights_path
             ))
 
     def _set_optimizer(self):
@@ -217,8 +220,17 @@ class ImgEnhanceModel(BaseModel):
             full_img = cv2.cvtColor(full_img, cv2.COLOR_RGB2BGR)
             cv2.imwrite(os.path.join(self.sample_dir, f'{iteration:06d}.png'), full_img)
     
-    def test(self, test_dl: DataLoader):
+    def test(self, test_dl: DataLoader, epoch: int, test_name: str):
         assert self.mode == 'test', f"The mode must be 'test', but got {self.mode}"
+        
+        result_dir = os.path.join(self.result_dir, test_name, str(epoch))
+        if os.path.exists(result_dir):
+            shutil.rmtree(result_dir)
+        os.makedirs(result_dir)
+        os.makedirs(os.path.join(result_dir, 'paired'))
+        os.makedirs(os.path.join(result_dir, 'single/input'))
+        os.makedirs(os.path.join(result_dir, 'single/predicted'))
+
         t_elapse_list = []
         idx = 1
         psnr_val_list = []
@@ -237,27 +249,39 @@ class ImgEnhanceModel(BaseModel):
                 t_elapse_avg = (time.time() - t_start) / num
                 t_elapse_list.append(t_elapse_avg)
 
-                # record metrics values
-                psnr_val = psnr(pred_imgs, ref_imgs, 1.0).item()
-                ssim_val = ssim(pred_imgs, ref_imgs, 11, 1.0).mean().item()
-                psnr_val_list.append(psnr_val)
-                ssim_val_list.append(ssim_val)
-
-                # record visual results
+                # record visual results and metrics values
                 full_img = self._gen_comparison_img(inp_imgs, pred_imgs, ref_imgs)
                 full_img = cv2.cvtColor(full_img, cv2.COLOR_RGB2BGR)
-                cv2.imwrite(os.path.join(self.result_dir, 'paired', f'{idx:06d}.png'), full_img)
-                with open(os.path.join(self.result_dir, 'paired', f"{idx:06d}.txt"), 'w') as f:
+                cv2.imwrite(os.path.join(result_dir, 'paired', f'{idx:06d}.png'), full_img)
+                with open(os.path.join(result_dir, 'paired', f"{idx:06d}.txt"), 'w') as f:
                     f.write('\n'.join(img_names))
+                ssim_val = ssim(pred_imgs, ref_imgs, 11, 1.0).mean().item()
+                ssim_val_list.append(ssim_val)
                 for (img_name, inp_img, pred_img, ref_img) in zip(
                     img_names, inp_imgs, pred_imgs, ref_imgs):
-                    save_image(inp_img.data, os.path.join(self.result_dir, 'single/input', img_name))
-                    save_image(pred_img.data, os.path.join(self.result_dir, 'single/predicted', img_name))
+                    psnr_val = psnr(pred_img, ref_img, 1.0).item()
+                    psnr_val_list.append(psnr_val)
+                    save_image(inp_img.data,
+                               os.path.join(result_dir, 'single/input', img_name))
+                    save_image(pred_img.data,
+                               os.path.join(result_dir, 'single/predicted', img_name))
             idx += 1
 
         frame_rate = 1 / (sum(t_elapse_list) / len(t_elapse_list))
         psnr_val = sum(psnr_val_list) / len(psnr_val_list)
         ssim_val = sum(ssim_val_list) / len(ssim_val_list)
+
+        with open(
+            os.path.join(result_dir, 'metrics.csv'),
+            'w') as f:
+            f.write('frame_rate,psnr,ssim\n'
+                    '{:.1f},{:.3f},{:.3f}'.format(frame_rate, psnr_val, ssim_val))
+        if self.logger:
+            self.logger.info(
+                'framte_rate: {:.1f} fps, psnr: {:.3f}, ssim: {:.3f}'.format(
+                    frame_rate, psnr_val, ssim_val
+                )
+            )
 
         return (frame_rate, psnr_val, ssim_val)
     
