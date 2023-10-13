7877115fef731a02cfc16e30d49d6702c261962d
fansuregrin <pwz113436@gmail.com>
2023-10-13 12:01:11 +0800

    update wfefb and ranet3

--- networks/wfefb.py
+++ networks/wfefb.py
@@ -2,6 +2,31 @@ import torch.nn as nn
 import torch
 
 
+class WFEFB(nn.Module):
+    def __init__(self, in_dim, out_dim, k_size=3):
+        super().__init__()  
+        self.norm = nn.InstanceNorm2d(in_dim)
+        self.conv1 = nn.Conv2d(in_dim, out_dim, 1, 1, 0)
+        self.conv2 = nn.Conv2d(in_dim, out_dim, 1, 1, 0)
+        self.conv3 = nn.Conv2d(in_dim, out_dim, 1, 1, 0)
+        self.conv4 = nn.Conv2d(in_dim, out_dim, 1, 1, 0)
+        self.conv5 = nn.Conv2d(in_dim, out_dim, 1, 1, 0)
+        self.sin_conv = nn.Conv2d(out_dim, out_dim, kernel_size=k_size, stride=1, padding=k_size//2)
+        self.cos_conv = nn.Conv2d(out_dim, out_dim, kernel_size=k_size, stride=1, padding=k_size//2)
+        self.last_conv = nn.Conv2d(out_dim*3, out_dim, 1, 1)
+ 
+    def forward(self, x):
+        x = self.norm(x)
+        cos_f = self.conv1(x) * torch.cos(self.conv2(x))
+        cos_f = self.cos_conv(cos_f)
+        x_f = self.conv3(x)
+        sin_f = self.conv5(x) * torch.sin(self.conv4(x))
+        sin_f = self.sin_conv(sin_f)
+        out = self.last_conv(torch.cat((cos_f, x_f, sin_f), dim=1))
+
+        return out
+
+
 class MLP(nn.Module):
     def __init__(self, in_features, hidden_features, out_features, activ=nn.ReLU):
         super().__init__()
@@ -39,7 +64,7 @@ class SAWF(nn.Module):
         return attn
 
 
-class WFEFB(nn.Module):
+class WFEFB2(nn.Module):
     def __init__(self, in_dim, out_dim, k_size=3):
         super().__init__()  
         self.norm = nn.InstanceNorm2d(in_dim)
