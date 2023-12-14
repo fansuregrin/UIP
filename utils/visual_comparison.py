c07e2b3402fa4cc95cc6844453e1afacb9378b09
fansuregrin <pwz113436@gmail.com>
2023-12-14 15:51:48 +0800

    update visual comparison

--- utils/visual_comparison.py
+++ utils/visual_comparison.py
@@ -53,8 +53,9 @@ def gen_comparison_with_local_mag(
         img_name_list,
         local_areas,
         img_dirs,
-        font_size=28,
+        font_cfg=None,
         expected_size=(256, 256),
+        title_y=-0.2,
         outline_color=(255,255,0),
         outline_width=3,
         save_fig=False,
@@ -64,12 +65,9 @@ def gen_comparison_with_local_mag(
     num_rows = len(img_name_list)
     num_cols = len(img_dirs)
 
-    fig, axes = plt.subplots(num_rows, num_cols, figsize=(num_cols*3*2, num_rows*3.5))
-
-    label_font = {
-        'fontfamily': 'Nimbus Roman',
-        'fontsize': font_size,
-    }
+    fig_width = num_cols * (2*expected_size[0]/100*(1+0.1)) 
+    fig_height = num_rows * (expected_size[1]/100*(1+0.1))
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=(fig_width, fig_height))
 
     if not isinstance(img_dirs, OrderedDict):
         img_dirs = OrderedDict(img_dirs)
@@ -90,8 +88,8 @@ def gen_comparison_with_local_mag(
             full_img = np.concatenate((img, local_mag), 1)
             ax = axes[i, j]
             ax.axis('off')
-            if i == 0:
-                ax.set_title(label, **label_font)
+            if i == num_rows-1:
+                ax.set_title(label, fontdict=font_cfg, y=title_y)
             ax.imshow(full_img)
     fig.tight_layout()
     if not save_fig:
