8f2ca9aa4feea3db5f973faefe13352f436fc93b
fansuregrin <pwz113436@gmail.com>
2024-12-18 16:43:35 +0800

    feat: enhance argument parsing for reference evaluation script

--- scripts/ref_eval_m.sh
+++ scripts/ref_eval_m.sh
@@ -2,56 +2,131 @@ script_dir=$(dirname $0)
 proj_dir=$(dirname ${script_dir})
 source ${script_dir}/ansi_escape.sh
 
+
+################## parse arguments and options ###############################
+short_opt=h,t
+long_opt=help,load_prefix:,width:,height:,resize,output_format:,window_size:,dry_run
+options=$(getopt -a --options ${short_opt} --longoptions ${long_opt} -- "$@")
+# echo ${options}
+
+eval set -- "${options}"
+
+help_str="
+Usage:
+ $0 model_name network name epochs [options]
+
+Reference-based Evaluation.
+
+Positional Arguments:
+ model_name                     name of the model
+ network                        name of the network
+ name                           name of traing progress
+ epochs                         epochs
+
+Options:
+ --load_prefix <load_prefix>    prefix of the filename of weight file                  
+ --width <width>                image width to resize
+ --height <height>              image height to resize
+ --resize                       resize image
+ --output_format <format>...    format for saving evaluation data
+ --window_size <window_size>    window size for calculating ssim
+ -t, --dry_run                  just test not run actually
+ -h, --help                     display this help
+"
+
+load_prefix=weights
+width=256
+height=256
+resize=
+output_format="csv pkl"
+window_size=11
+dry_run=
+
+while true; do
+  case "$1" in
+    --width)
+      width="$2"
+      shift 2
+      ;;
+    --height)
+      height="$2"
+      shift 2
+      ;;
+    --resize)
+      resize="--resize"
+      shift 1
+      ;;
+    --output_format)
+      output_format="$2"
+      shift 2
+      ;;
+    --window_size)
+      window_size="$2"
+      shift 2
+      ;;
+    -t | --dry_run)
+      dry_run="echo "
+      shift 1
+      ;;
+    -h | --help)
+      echo -e "${help_str}"
+      exit 0
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "Unexpected option: $1"
+      ;;
+  esac
+done
+
+if [ $# -lt 4 ];then
+    echo -e "${help_str}"
+    exit 1
+fi
+
+model_name=${1}
+net=${2}
+name=${3}
+epochs=${4}
+################## parse arguments and options ###############################
+
+
 declare -A refer_dict
 refer_dict=([EUVP515]="/DataA/pwz/workshop/Datasets/EUVP_Dataset/test_samples/GTr"
             [OceanEx]="/DataA/pwz/workshop/Datasets/ocean_ex/good"
             [UIEB100]="/DataA/pwz/workshop/Datasets/UIEB100/reference"
             [LSUI]="/DataA/pwz/workshop/Datasets/LSUI/test/ref")
 
-if [ $# -lt 5 ]
-then
-    echo -e "${RED}PLEASE PASS IN THE FOLLOWING ARGUMENTS IN ORDER!${ENDSTYLE}"
-    echo -e "1) model_v"
-    echo -e "2) net"
-    echo -e "3) name"
-    echo -e "4) epochs: multiple epochs must be separated by commas"
-    echo -e "5) load_prefix"
-    echo -e "for example: \"${BOLD_GREEN}bash ${0} ie ra LSUI_01 299 weights${ENDSTYLE}\""
-    exit -1
-fi
-model_v=${1}
-net=${2}
-name=${3}
-raw_epochs=${4}
-epochs=(${raw_epochs//,/ })
-load_prefix=${5}
-
+epoch_arr=(${epochs})
 for ds_name in ${!refer_dict[@]}
 do
-    for epoch in ${epochs[*]}
+    for epoch in ${epoch_arr[*]}
     do
-        target_dir="results/${model_v}/${net}/${name}/${ds_name}/${load_prefix}_${epoch}"
+        target_dir="${proj_dir}/results/${model_name}/${net}/${name}/${ds_name}/${load_prefix}_${epoch}"
         if [ -d ${target_dir} ]
         then
-            python ${proj_dir}/ref_eval_pd.py \
+            ${dry_run}python ${proj_dir}/ref_eval_pd.py \
                 -inp "${target_dir}/single/predicted" \
                 -ref "${refer_dict[${ds_name}]}" \
                 -out "${target_dir}" \
-                --resize \
-                # --width 224 --height 224 \
+                ${resize} --width ${width} --height ${height} \
+                --output_format ${output_format} \
+                --window_size ${window_size}
         else
             echo -e "${RED}[${target_dir}]${ENDSTYLE} not exist!"
         fi
     done
 done
 
-epochs_space_sep=$(echo ${raw_epochs} | tr ',' ' ')
 ds_names=$(echo "${!refer_dict[@]}")
-python ${script_dir}/get_eval_m.py \
-    ${model_v} \
+${dry_run}python ${script_dir}/get_eval_m.py \
+    ${model_name} \
     ${net} \
     ${name} \
-    ${epochs_space_sep} \
+    ${epochs} \
     --eval_type ref \
     --ds_names ${ds_names} \
     --metric_names psnr ssim \
