e491fe4214dae3e28aa32f2ba6f64b23a723e891
fansuregrin <pwz113436@gmail.com>
2024-12-20 19:43:41 +0800

    feat: add dataset dictionary for WaterNet configurations

---
+++ scripts/ds_dict/ref_waternet.sh
@@ -0,0 +1,5 @@
+declare -A ds_dict
+ds_dict=([LSUI]="configs/dataset/lsui_waternet.yaml"
+         [UIEB100]="configs/dataset/uieb100_waternet.yaml"
+         [OceanEx]="configs/dataset/ocean_ex_waternet.yaml"
+         [EUVP515]="configs/dataset/euvp515_waternet.yaml")
