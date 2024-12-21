a84a36e9aafc89bb205ed375fc1be31477bb74e1
fansuregrin <pwz113436@gmail.com>
2024-12-21 21:21:53 +0800

    feat: refactor transform function to use a transformation table for cleaner code

--- data/transform.py
+++ data/transform.py
@@ -1,7 +1,17 @@
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
 from typing import Dict
 
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+
+TRANSFORMS_TBL = {
+    'resize': A.Resize,
+    'random_crop': A.RandomCrop,
+    'center_crop': A.CenterCrop,
+    'horizontal_flip': A.HorizontalFlip,
+    'vertical_flip': A.VerticalFlip,
+    'to_tensor': ToTensorV2
+}
 
 def get_transform(cfg: Dict | None = None):
     """Get albumentations transforms.
@@ -13,19 +23,12 @@ def get_transform(cfg: Dict | None = None):
         return ToTensorV2()
     transform_list = []
     for trans_opt in cfg.get('transforms', []):
-        if trans_opt['name'] == 'resize':
-            transf = A.Resize(trans_opt['height'], trans_opt['width'])
-        elif trans_opt['name'] == 'random_crop':
-            transf = A.RandomCrop(trans_opt['height'], trans_opt['width'])
-        elif trans_opt['name'] == 'center_crop':
-            transf = A.CenterCrop(trans_opt['height'], trans_opt['width'])
-        elif trans_opt['name'] == 'horizontal_flip':
-            transf = A.HorizontalFlip(p=trans_opt['p'])
-        elif trans_opt['name'] == 'to_tensor':
-            transf = ToTensorV2()
-        else:
-            assert False, f"'{trans_opt['name']}' is not supported!"
-        transform_list.append(transf)
+        name = trans_opt['name']
+        if name not in TRANSFORMS_TBL:
+            raise NotImplementedError(name)
+        args = trans_opt.copy()
+        args.pop('name')
+        transform_list.append(TRANSFORMS_TBL[name](**args))
     transforms = A.Compose(
         transform_list,
         additional_targets=cfg.get('additional_targets', None))
