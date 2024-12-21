e911db8977b15522f95f35450bd43fc54adf95dc
fansuregrin <pwz113436@gmail.com>
2024-12-21 17:16:50 +0800

    feat: add support for pretrained weights in FCN configuration

--- networks/fcn/fcn.py
+++ networks/fcn/fcn.py
@@ -1,18 +1,22 @@
 import torch.nn as nn
-from torchvision.models.segmentation import (
-    fcn_resnet50, fcn_resnet101
+from torchvision.models.segmentation.fcn import (
+    fcn_resnet50, fcn_resnet101, FCNHead
 )
 
 
 class FCN(nn.Module):
-    def __init__(self, num_classes, backbone, **kwargs):
+    def __init__(self, num_classes, backbone, use_pretrained=False, **kwargs):
         super().__init__()
+
+        weights = 'DEFAULT' if use_pretrained else None
         if backbone == 'resnet50':
-            self.fcn = fcn_resnet50(num_classes=num_classes)
+            self.fcn = fcn_resnet50(weights=weights)
         elif backbone == 'resnet101':
-            self.fcn = fcn_resnet101(num_classes=num_classes)
+            self.fcn = fcn_resnet101(weights=weights)
         else:
-            assert 'not implement'
+            raise NotImplementedError()
+        
+        self.fcn.classifier = FCNHead(2048, num_classes)
 
     def forward(self, x):
         return self.fcn(x)['out']
