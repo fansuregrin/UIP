50a964e0f319a5e5e38c9b423631e927c385d31b
fansuregrin <pwz113436@gmail.com>
2024-12-22 22:15:57 +0800

    feat: update faster_rcnn network and configs for faster_rcnn and deeplabv3

--- networks/faster_rcnn/faster_rcnn.py
+++ networks/faster_rcnn/faster_rcnn.py
@@ -7,14 +7,18 @@ from torchvision.models.detection.faster_rcnn import (
 
 
 class FasterRCNN(nn.Module):
-    def __init__(self, version, num_classes, *args, **kwargs):
+    def __init__(self, version, num_classes, use_pretrained=False, **kwargs):
         super().__init__()
+        
+        weights = 'DEFAULT' if use_pretrained else None
+        
         if version == 'fasterrcnn_resnet50_fpn_v2':
-            self.faster_rcnn = fasterrcnn_resnet50_fpn_v2()
+            self.faster_rcnn = fasterrcnn_resnet50_fpn_v2(weights=weights)
         elif version == 'fasterrcnn_mobilenet_v3_large_fpn':
-            self.faster_rcnn = fasterrcnn_mobilenet_v3_large_fpn()
+            self.faster_rcnn = fasterrcnn_mobilenet_v3_large_fpn(weights=weights)
         else:
             raise NotImplementedError()
+        
         in_features = self.faster_rcnn.roi_heads.box_predictor.cls_score.in_features
         self.faster_rcnn.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
     
